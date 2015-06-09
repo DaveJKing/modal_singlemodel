1ac858527581bafe807fb4c357262dd909a579b4
Dave King <djking@glam.ac.uk>
2015-06-09 09:48:30 +0100

    first commit bootstrap working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Modals::Application.config.secret_key_base = '9ea76266453e3141840416a1746644d1a18f9125a78e7cafa558b84c6ba0767ef908e07a243d65098d34909d2ac40747a1dcb1ee606e491958aed7d4dad0afce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Modals::Application.config.session_store :cookie_store, key: '_modals_session'
